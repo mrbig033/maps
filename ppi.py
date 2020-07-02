ac59185eec40696d60f283a2c5f24fee0d8f07d4
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-02 18:22:46 -0300

    automatic commit

---
+++ ppi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def CalculatePi(roundVal):
+
+    somepi = round(math.pi, roundVal)
+    return somepi
+
+
+roundTo = input("Enter the number of digits you want after the decimal for Pi: ")
+try:
+    roundint = int(roundTo)
+    print(CalculatePi(roundint))
+except:
+    print("You did not enter an integer")
